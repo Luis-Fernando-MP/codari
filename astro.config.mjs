5eebec883c0bb051a011bf5cb2891ba65690c911
Haui <luigfmp@gmail.com>
2025-07-16 19:05:53 -0500

    build: :package: Refactor code structure for improved readability and maintainability

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
 import { defineConfig } from 'astro/config'
 import tailwindcss from '@tailwindcss/vite'
+import vercel from '@astrojs/vercel/serverless'
 
-
-import react from '@astrojs/react';
+import react from '@astrojs/react'
 
 export default defineConfig({
-  vite: {
-      plugins: [tailwindcss()]
+	output: 'server',
+	vite: {
+		plugins: [tailwindcss()]
 	},
-
-  integrations: [react()]
+	integrations: [react()],
+	// 👀 look here
+	adapter: vercel()
 })
